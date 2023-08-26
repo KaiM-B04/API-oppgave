c95be8cf4eb56be1ae5d68c259205c117f8bbab7
kai <kaim.kodehode@gmail.com>
2023-08-26 20:21:53 +0200

    Endelig ferdig

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # API-oppgave
 
 README fil oppdatert
+
+INFO TIL LESER:
+
+Jeg har prøvde å lage til en litt mer avansert side, hvor man kunne velge hver film og lese mer om dem, og kunne søke på navn fra filmene for å finne filmen de er i, men det ser ut til at søke-funksjonen ikke er en del av API-en.
+
+Jeg ønsker å komme tilbake til denne oppgaven, for å ha den skikkelig satt opp til portfolioen.
--- js/script.js
+++ js/script.js
@@ -25,23 +25,31 @@ async function displayFilms() {
     filmElement.innerHTML = `
     <h3 class="title">${film.title}</h3>
     <p class="info"><strong>Description:</strong> ${film.description}</p>
-`;
+    <button class="details-button" data-filmid="${film.id}">Details</button>
+    <div class="film-details" id="film-details-${film.id}" style="display: none;">
+      <p><b>Original Title: </b>${film.original_title} <br><i>${film.original_title_romanised}</i></p>
+      <p><b>Run Time: </b>${film.running_time} min</p>
+      <p><b>Released: </b>${film.release_date}</p>
+      <p><b>Director: </b>${film.director}</p>
+      <p><b>Producer: </b>${film.producer}</p>
+      <button class="close">Close</button>
+    </div>
+  `;
+
+    const detailsButton = filmElement.querySelector(".details-button");
+    const close = filmElement.querySelector(".close");
+    const filmDetails = filmElement.querySelector(`#film-details-${film.id}`);
+
+    detailsButton.addEventListener("click", () => {
+      filmDetails.style.display = "block";
+    });
+
+    close.addEventListener("click", () => {
+      filmDetails.style.display = "none";
+    });
 
     mainContainer.appendChild(filmElement);
   });
 }
 
 displayFilms();
-
-// async function moreInfo() {
-//   const info = await getFilms();
-//   const button = document.getElementsByClassName("film");
-
-//   info.forEach(() => {
-//     const readMore = document.createElement("div");
-//     readMore.classList.add("mroe");
-
-//     readMore.innerHTML = `
-//     `;
-//   });
-// }
